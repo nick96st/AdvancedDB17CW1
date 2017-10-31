c0ea809362955579ffa80175fdd783ecbc36aaac
ny1215 <ny1215@ic.ac.uk>
2017-10-31 19:38:01 +0000

    upload

--- ADBCoursework1.cpp
+++ ADBCoursework1.cpp
@@ -35,8 +35,7 @@ std::vector<std::string> findHours(odb::database& db, std::string username) {
 	r_result_t reviews_objs(db.query(get_reviews_with_username));
 	for (auto obj:reviews_objs) {
 		auto obj_hour = obj.get_business_id()->get_hour_ptr();
-		auto res = obj_hour.load();
-		std::cout<<res.get_hours()<<endl;
+		std::cout<<obj_hour->get_hours()<<endl;
 	}
 
 //	b_query_t get_businesses_from_reviews_with_username(b_query_t::review_id->user_id->name == username);
