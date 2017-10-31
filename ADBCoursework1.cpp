0f3b788de3f4125b022e1aaa8aadeaa7c1a4c360
ny1215 <ny1215@ic.ac.uk>
2017-10-31 21:08:59 +0000

    upload

--- ADBCoursework1.cpp
+++ ADBCoursework1.cpp
@@ -54,8 +54,9 @@ std::vector<StarCount> countStars(odb::database& db, float latMin, float latMax,
 	std::vector<StarCount> result;
 	transaction t(db.begin());
 	// Your implementation goes here:
-	sc_result_t results = db.query<StarCount>(std::string("SELECT stars, COUNT(*) FROM review") +
-													  " GROUP BY review.stars");
+	sc_result_t results = db.query<StarCount>(std::string("SELECT stars FROM review,business WHERE review.business_id = business.id AND ") +
+													  "business.longitude BETWEEN " + to_string(longMin) + " AND " + to_string(longMax) +
+													  "AND business.latitude BETWEEN " +  to_string(latMin) + " AND " + to_string(latMax));
 	for(auto item : results) {
 		std::cout<<item.stars<<":"<<item.count<<endl;
 		result.push_back(item);
